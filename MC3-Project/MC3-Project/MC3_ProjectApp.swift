12fd521c09092562a4f48de642267138fbbeadfd
Kiwi <toskw@naver.com>
2023-07-13 21:05:17 +0900

    add: 프로젝트 파일 추가

---
+++ MC3-Project/MC3-Project/MC3_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3_ProjectApp.swift
+//  MC3-Project
+//
+//  Created by 송기원 on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct MC3_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
